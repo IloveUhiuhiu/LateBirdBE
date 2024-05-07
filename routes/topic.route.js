22d17e4a0566342ca68cf7b7212242290bc3875b
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-05-07 09:02:05 +0700

    fix route topic result

--- routes/topic.route.js
+++ routes/topic.route.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const topicController = require("../controllers/topic.controller");
 
-router.get("/hi", topicController.hi);
 router.get("/information", topicController.getTopicById);
 router.get("/allinformation",topicController.getAllTopic);
 router.post("/create", topicController.createTopic);
-router.post("/update", topicController.updateTopic);
+router.put("/update", topicController.updateTopic);
 module.exports = router;
